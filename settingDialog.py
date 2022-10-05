1cdcb6b56a196bedc93e72d4f15cfc988ef9ab5b
liuxsdev <1028330144@qq.com>
2022-10-05 21:52:53 +0800

    add setting dialog

---
+++ settingDialog.py
@@ -0,0 +1,20 @@
+import os
+
+from qgis.PyQt import uic
+from qgis.PyQt import QtWidgets
+
+# This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'ui/setting.ui'))
+
+
+class settingDialog(QtWidgets.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(settingDialog, self).__init__(parent)
+        # Set up the user interface from Designer through FORM_CLASS.
+        # After self.setupUi() you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
